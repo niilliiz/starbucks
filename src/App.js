06a556847afb535c68e896418c2e7e63e8e7ad69
niilliiz <niloo.vatan.nv@gmail.com>
2022-12-11 16:45:50 +0330

    feat: tailwind installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
