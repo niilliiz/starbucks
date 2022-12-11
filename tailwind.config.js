ed1703f1ac17d81728689eb30f7f6163b3b1b2cc
niilliiz <niloo.vatan.nv@gmail.com>
2022-12-11 18:00:20 +0330

    feat:  screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "640em",
+      md: "768em",
+      lg: "1024em",
+      xl: "1280em",
+    },
     extend: {},
   },
   plugins: [],
