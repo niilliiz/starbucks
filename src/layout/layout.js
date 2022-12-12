f35482ed7bd06760bce96b45847ce011ea6e5ae1
niilliiz <niloo.vatan.nv@gmail.com>
2022-12-12 18:30:59 +0330

    feat: layout

---
+++ src/layout/layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../sections/header/header";
+const Layout = () => {
+  return (
+    <div className="relative max-w-screen-xl mx-auto my-0">
+      <Header />
+    </div>
+  );
+};
+export default Layout;
