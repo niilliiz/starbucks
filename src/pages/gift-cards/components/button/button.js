ea2da6a42bcd6425b80baae047f93972a3b96f6c
niilliiz <niloo.vatan.nv@@gmail.com>
2023-05-12 12:02:51 +0330

    fix: button position

--- src/pages/gift-cards/components/button/button.js
+++ src/pages/gift-cards/components/button/button.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { CaretLeft, CaretRight } from "phosphor-react";
+import React from 'react';
+import { CaretLeft, CaretRight } from 'phosphor-react';
 
-function Button({ direction, onClick }) {
+function Button({ direction, onClick, left, right }) {
   return (
     <div
-      className={`hidden flex-col items-center justify-center absolute ${direction}-6  -translate-y-2/4 bg-gray-200 w-11 h-11 rounded-circle shadow-sm top-2/4  cursor-pointer  z-10 transition-opacity hover:opacity-95 sm:flex`}
+      className={` hidden flex-col items-center justify-center  absolute ${left} ${right} -translate-y-2/4 bg-gray-200 w-11 h-11 rounded-circle shadow-sm top-2/4   cursor-pointer  z-10 transition-opacity hover:opacity-95 sm:flex`}
       onClick={onClick}
     >
-      {direction === "left" ? (
+      {direction === 'left' ? (
         <CaretLeft size={24} />
       ) : (
         <CaretRight size={24} />
--- src/pages/gift-cards/components/categories/categories.js
+++ src/pages/gift-cards/components/categories/categories.js
@@ -1,30 +1,28 @@
-import React, { useState, useRef } from "react";
-import Button from "../button/button";
-import Carousel from "../carousel/carousel";
+import React, { useRef } from 'react';
+import Button from '../button/button';
+import Carousel from '../carousel/carousel';
 
 const Categories = ({ category }) => {
-  const [visible, setVisible] = useState(true);
-
   const carouselRef = useRef(null);
 
   const handleClick = (e, dir) => {
     const sliderIndex = parseInt(
-      getComputedStyle(carouselRef.current).getPropertyValue("--slider-index")
+      getComputedStyle(carouselRef.current).getPropertyValue('--slider-index')
     );
     const itemCounts = carouselRef.current.children.length;
     const itemPerScreen = parseInt(
       getComputedStyle(carouselRef.current).getPropertyValue(
-        "--items-per-screen"
+        '--items-per-screen'
       )
     );
     const unit = Math.ceil(itemCounts / itemPerScreen);
 
-    if (dir === "left") {
+    if (dir === 'left') {
       if (sliderIndex - 1 < 0) {
-        carouselRef.current.style.setProperty("--slider-index", +unit - 1);
+        carouselRef.current.style.setProperty('--slider-index', +unit - 1);
       } else {
         carouselRef.current.style.setProperty(
-          "--slider-index",
+          '--slider-index',
           +sliderIndex - 1
         );
       }
@@ -32,12 +30,12 @@ const Categories = ({ category }) => {
 
     // joda kon ghesmate define unut rom unit mitune state bashe, baraye neshun dadane button ha bayad ye jur dg benevisi
 
-    if (dir === "right") {
+    if (dir === 'right') {
       if (sliderIndex + 1 >= unit) {
-        carouselRef.current.style.setProperty("--slider-index", 0);
+        carouselRef.current.style.setProperty('--slider-index', 0);
       } else {
         carouselRef.current.style.setProperty(
-          "--slider-index",
+          '--slider-index',
           +sliderIndex + 1
         );
       }
@@ -45,15 +43,23 @@ const Categories = ({ category }) => {
   };
 
   return (
-    <section className="flex flex-col space-y-4 px-6">
-      <div className="flex justify-between mx-3">
-        <h2 className="text-20 font-700 text-green-900">{category.title}</h2>
-        <button className="text-green-700 font-500">See all</button>
+    <section className='flex flex-col space-y-4 px-6'>
+      <div className='flex justify-between mx-3'>
+        <h2 className='text-20 font-700 text-green-900'>{category.title}</h2>
+        <button className='text-green-700 font-500'>See all</button>
       </div>
-      <div className="relative w-full h-56 overflow-hidden">
-        <Button direction="left" onClick={(e) => handleClick(e, "left")} />
+      <div className='relative w-full h-56 overflow-hidden'>
+        <Button
+          direction='left'
+          left='left-6'
+          onClick={(e) => handleClick(e, 'left')}
+        />
         <Carousel gifts={category.gifts} ref={carouselRef} />
-        <Button direction="right" onClick={(e) => handleClick(e, "right")} />
+        <Button
+          direction='right'
+          right='right-6'
+          onClick={(e) => handleClick(e, 'right')}
+        />
       </div>
     </section>
   );
